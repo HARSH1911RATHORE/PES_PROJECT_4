6ad601dea60f2309c9a5a805314879bc8e8fff91
HARSH1911RATHORE <55412102+HARSH1911RATHORE@users.noreply.github.com>
2019-11-05 04:00:00 -0700

    Add files via upload

--- I2C_PES_PROJECT4_1.h
+++ I2C_PES_PROJECT4_1.h
@@ -19,11 +19,16 @@
 #define I2C_M_STOP    I2C0->C1 &= ~I2C_C1_MST_MASK
 #define I2C_M_RSTART  I2C0->C1 |= I2C_C1_RSTA_MASK
 
-#define I2C_TRAN      I2C0->C1 |= I2C_C1_TX_MASK;
-#define I2C_REC       I2C0->C1 &= ~I2C_C1_TX_MASK;
+#define I2C_TRAN      I2C0->C1 |= I2C_C1_TX_MASK
+#define I2C_REC       I2C0->C1 &= ~I2C_C1_TX_MASK
 
-#define I2C_WAIT      while((I2C0->S & I2C_S_IICIF_MASK)==0) {} \
-					I2C0->S |= I2C_S_IICIF_MASK;
+#define I2C_WAIT      while((I2C0->S & I2C_S_IICIF_MASK)==0) \
+								{\
+										/*PRINTF("\n \r %d",counter2);*/  \
+											counter2++;\
+											if (counter2==5000){nack_f=1;break;}\
+								 } \
+									I2C0->S |= I2C_S_IICIF_MASK;
 
 
 #define NACK  I2C0->C1 |= I2C_C1_TXAK_MASK
@@ -35,22 +40,24 @@
 #define temp_low_register  0x02
 
 #define temp_addr  0x00
-#define Temp_high_byte1 0x18
+#define Temp_high_byte1 0x19
 #define Temp_high_byte2 0x00
 
-#define Temp_low_byte1 0x14
-#define Temp_low_byte2 0x96
+#define Temp_low_byte1 0x18
+#define Temp_low_byte2 0x00
 
-#define Config_byte1 0x0C
+
+
+#define Config_byte1 0x6C
 #define Config_byte2 0xA0
 
-#define MASK(x)    (1UL << (x))
-#define RED_LED    18
+//#define MASK(x)    (1UL << (x))
+//#define RED_LED    18
 
 
 void i2c_init(void);
 void i2c_write_byte(uint8_t dev, uint8_t reg, uint8_t data_byte1,uint8_t data_byte2);
-int i2c_read_bytes(uint8_t dev_adx,uint8_t reg_adx);
+int i2c_read_bytes(uint8_t dev_adx,uint8_t reg_adx,int CR);
 void i2c_read_byte(uint8_t dev, uint8_t reg);
 
 #endif
